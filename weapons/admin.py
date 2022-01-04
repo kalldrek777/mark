8d12c587bdde70ebc30e960ebb52ff75f7b9a67a
Market <kalldrek@yandex.ru>
2022-01-04 16:06:19 +0300

    авыа

--- weapons/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from weapons.models import Product
-
-admin.site.register(Product)
--- weapons/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class SearchForm(forms.Form):
-    query = forms.CharField(label="", max_length=20, widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Название товара'
-    }))
