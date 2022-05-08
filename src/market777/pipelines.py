4a617bbdedbb080ea79aaad66900db7e04805ba0
Market <kalldrek@yandex.ru>
2022-05-08 17:02:41 +0300

    'dic'

--- src/market777/pipelines.py
+++ src/market777/pipelines.py
@@ -59,7 +59,7 @@ class Market777Pipeline(object):
     def store_db(self, item):
         qs = Product.objects.all()
 
-        self.curr.execute('''insert into example(src) VALUES''', (
+        self.curr.execute('''insert into example(src) VALUES(%s)''', (
             item['img_url'],
         ))
         self.curr.execute('''UPDATE weapons_product SET
