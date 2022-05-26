21e7dba8714c758ffc76894d657904170c7a37b6
kalldrek777 <79919082+kalldrek777@users.noreply.github.com>
2022-05-26 15:08:22 +0300

    Update production.py

--- src/Market/settings/production.py
+++ src/Market/settings/production.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-s37q8t$xns=&!b1u$xtqd#$lk2li&b!hi@nvlvibe-$v+zckq9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
